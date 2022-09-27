0782e044aaf4301ee7df159dfef23c4db61dbbde
Kent Henneuse <kent@henneuse.org>
2022-09-27 12:01:44 -0700

    Add a config service
    Creating a service the can be used to grab configurations for the
    application. This is also a chance to look into different approaches
    for the way to get the configuration to the TypeORM cli for builing
    entities and especially migrations.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { LoanApplicationsService } from './loan-applications/loan-applications.service';
 import { LoanApplicationsController } from './loan-applications/loan-applications.controller';
+import { ConfigService } from './config/config.service';
 
 @Module({
   imports: [],
   controllers: [AppController, UsersController, LoanApplicationsController],
-  providers: [AppService, UsersService, LoanApplicationsService],
+  providers: [AppService, UsersService, LoanApplicationsService, ConfigService],
 })
 export class AppModule {}
