0d42292ea47af4ecdfcec2fab522c25156c9bab9
Kent Henneuse <kent@convoy.com>
2022-09-27 09:35:41 -0700

    Implement the /users and /users/:id routes
    Built out the routes and tests to confirm that we are getting back
    data.  These are brittle tests because they use direct ids and names
    for the users.  This should be corrected in a future PR.

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -7,6 +8,7 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +16,26 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('/users', () => {
+    it('should get all the users', () => {
+      const users = controller.getAllUsers();
+      expect(users).toHaveLength(5);
+    });
+  });
+
+  describe('/users/id', () => {
+    it('should get a specific user if id found', () => {
+      const user = controller.getUserById(
+        'fb414ddc-6e3b-4859-bb45-56d7389a64ba',
+      );
+      expect(user).toBeDefined();
+      expect(user.name).toBe('Adam');
+    });
+
+    it('should return undefined if the id is not found', () => {
+      expect(controller.getUserById('BOGUS')).toBeUndefined();
+    });
   });
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserDTO, UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string): UserDTO | undefined {
+    return this.usersService.getUserById(id);
+  }
+}
